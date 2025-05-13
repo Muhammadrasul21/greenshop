9441ac7ecaab64ede33c9fbd70c3eb7007991396
Muhammadrasul21 <theboy6878@gmail.com>
2025-05-13 15:01:35 +0500

    fixed cart

--- src/pages/home/Cart.jsx
+++ src/pages/home/Cart.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeFromCart } from "../../redux/cartSlice";
+import Products from "../shop/Products";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.items);
@@ -11,31 +12,34 @@ const Cart = () => {
   };
 
   return (
-    <div className="p-4 container">
-      <h2 className="text-xl font-bold mb-4">Savatchadagi Mahsulotlar</h2>
-
-      {cartItems.length === 0 ? (
-        <p>Savatcha hozircha bo'sh.</p>
-      ) : (
-        <ul className="space-y-2">
-          {cartItems.map((item) => (
-            <li key={item.id} className="border p-3 rounded shadow-sm">
-              <p>
-                <strong>{item.name}</strong>
-              </p>
-              <p>Narxi: ${item.price}</p>
-              <p>Miqdori: {item.quantity}</p>
-              <button
-                onClick={() => handleRemove(item.id)}
-                className="mt-2 px-3 py-1 text-sm bg-red-500 text-white rounded hover:bg-red-600"
-              >
-                O‘chirish
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <div className="p-4 container">
+        <h2 className="text-xl font-bold mb-4">Savatchadagi Mahsulotlar</h2>
+  
+        {cartItems.length === 0 ? (
+          <p>Savatcha hozircha bo'sh.</p>
+        ) : (
+          <ul className="space-y-2">
+            {cartItems.map((item) => (
+              <li key={item.id} className="border p-3 rounded shadow-sm">
+                <p>
+                  <strong>{item.name}</strong>
+                </p>
+                <p>Narxi: ${item.price}</p>
+                <p>Miqdori: {item.quantity}</p>
+                <button
+                  onClick={() => handleRemove(item.id)}
+                  className="mt-2 px-3 py-1 text-sm bg-red-500 text-white rounded hover:bg-red-600"
+                >
+                  O‘chirish
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <Products/>
+    </>
   );
 };
 
